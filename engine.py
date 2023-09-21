d31461dbb135055e4427fdedb3f83184b3c3f277
dkrivaa <dannykrivaa@hotmail.com>
2023-09-21 16:34:59 +0300

    Arrivals and Departures

--- engine.py
+++ engine.py
@@ -58,13 +58,16 @@ def delays_depart():
 
 def delays_arrive():
     df = get_data()
-    dfa = df.loc[(df['CHAORD'] == 'A') & (df['CHRMINE'] == 'LANDED')]
-
-    average_delay_arrive = dfa['delay'].mean()
-    average_delay_by_airline = dfa.groupby(df['CHOPERD'])['delay'].mean()
+    dfa1 = df.loc[(df['CHAORD'] == 'A') & (df['CHRMINE'] == 'LANDED') & (df['delay'] > pd.Timedelta(seconds=0))]
+    dfa2 = df.loc[(df['CHAORD'] == 'A') & (df['CHRMINE'] == 'LANDED') & (df['delay'] <= pd.Timedelta(seconds=0))]
+    average_delay_arrive = dfa1['delay'].mean()
+    average_early_arrival = -(dfa2['delay'].mean())
+    average_delay_by_airline = dfa1.groupby(df['CHOPERD'])['delay'].mean()
     print('Average arrival delay last 24 hours: ', average_delay_arrive)
     print('arrivals: ', average_delay_by_airline.nlargest(5))
     print('arrivals: ', average_delay_by_airline.nsmallest(5)[::-1])
+    print('Average early arrival last 24 hours: ', average_early_arrival)
+
 
 
 
